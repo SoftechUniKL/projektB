1083b9772b536204db117b5edec930266fa8c7e6
MaxJac <maxjac@examplefakeemaildoesntexist????.xxx>
2014-06-12 19:50:18 +0200

    Klassenstruktur
    Herumprobieren mit Klassen um Objektorientierung herzustellen/näher zu
    kommen.
    Problem: Zugriff auf Attribute der Kindklassen.
    Wie ziehe ich (automatisch) die TürFenster Fläche von Wand ab, etc. ?

---
+++ src/TürFenster.java
@@ -0,0 +1,13 @@
+public class TürFenster extends Wand {
+    double hoehe;
+    double breite;
+    double fläche;
+
+    public TürFenster() {
+    }
+
+    public TürFenster(double breite, double hoehe) {
+	this.hoehe = hoehe;
+	this.breite = breite;
+    }
+}
--- src/Wand.java
+++ src/Wand.java
@@ -1,13 +1,22 @@
-public class Wand {
+public class Wand extends Raum {
     double hoehe;
     double breite;
+    double fläche;
 
-    double flaeche() {
-	return this.hoehe * this.breite;
+    public Wand() {
+	TürFenster türFenster = new TürFenster();
     }
 
     public Wand(double breite, double hoehe) {
 	this.hoehe = hoehe;
 	this.breite = breite;
     }
+
+    double flaeche() {
+	this.fläche = (this.hoehe * this.breite) - türFenster.fläche;
+	// türfenster abzioehen?!?"ß;
+	return fläche;
+    }
+    // void newTürFenster(){
+    // TürFenster türFenster = new TürFenster();
 }
