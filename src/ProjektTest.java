95609a4838de87e2e0f655102fb2ae13d6c6d411
knoblo <knoblo@rhrk.uni-kl.de>
2014-06-26 16:08:01 +0200

    Test Wand
    Ich hab euch mal euren Test zu einem "Mustertest" umgeschrieben. Nach
    diesem Prinzip wird jeder Test bei JUnit durchgeführt.

--- src/ProjektTest.java
+++ src/ProjektTest.java
@@ -1,13 +1,27 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.rmi.RemoteException;
+
+import junit.framework.*;
 
 import org.junit.Test;
 
-public class ProjektTest {
-
+public class ProjektTest extends TestCase {
+    private Wand wand;
+    
+    protected void setUp() throws RemoteException {
+	wand = new Wand(8,6);
+    }
+    
+    protected void tearDown() throws RemoteException {
+	wand = null;
+    }
+    
     @Test
-    public void test() {
-	Wand wand = new Wand(8, 6);
+    public void test() throws RemoteException {
+	setUp();
 	assertEquals(48, wand.flaeche(), 0.002);
+	tearDown();
     }
 
 }
