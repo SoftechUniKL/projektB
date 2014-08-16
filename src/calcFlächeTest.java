66f8b06471622047245d013962a52643e5681a08
mhuber93 <marc_ch_124@yahoo.de>
2014-08-16 15:20:45 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/ProjektTest.java

--- src/calcFlächeTest.java
+++ src/calcFlächeTest.java
@@ -1,22 +1,25 @@
 import java.rmi.RemoteException;
 
-import org.junit.Assert;
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
-public class calcFlächeTest {
+public class calcFlächeTest extends TestCase {
 
     private Frame1 frame1;
     private Frame2 frame2;
-    private double fläche;
+    private double expectedFläche;
+    private double actualFläche;
     private double höhe;
     private double breite;
-    private double output;
+
+    // private double output;
 
     protected void setUp() throws RemoteException {
 
 	frame1 = new Frame1();
 	frame2 = new Frame2(frame1);
-	fläche = 35;
+	expectedFläche = 35;
 	höhe = 5;
 	breite = 7;
 	frame2.calcFläche(höhe, breite);
@@ -27,19 +30,24 @@ public class calcFlächeTest {
 
 	frame1 = null;
 	frame2 = null;
-	fläche = 0;
+	expectedFläche = 0;
 	breite = 0;
 	höhe = 0;
 
     }
 
     @Test
-    public void testCalcFläche() {
+    public void testCalcFläche() throws RemoteException {
 	setUp();
-	frame2.calcFläche(höhe, breite);
 
-	Assert.assertArrayEquals(35, frame2.calcFläche());
+	frame2.calcFläche(höhe, breite);
+	actualFläche = frame2.getFläche();
+	assertEquals(expectedFläche, actualFläche, 0.0002);
 	tearDown();
+
+	/*
+	 * ODER: assertEquals(expectedFläche, frame2.getFläche(), 0.0002
+	 */
     }
 
 }
