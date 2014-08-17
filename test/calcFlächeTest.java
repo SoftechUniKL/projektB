f5f357bf25b561405259e1f67bbbe61d9b2fc296
MaxJac <maxjac@examplefakeemaildoesntexist????.xxx>
2014-08-17 18:15:37 +0200

    Endlich test zu laufen gebracht!

---
+++ test/calcFlächeTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
+import java.rmi.RemoteException;
+
+import org.junit.Test;
+
+public class calcFlächeTest {
+
+    private Frame1 frame1;
+    private Frame2 frame2;
+    private double expectedFläche;
+    private double actualFläche;
+    private double höhe;
+    private double breite;
+
+    // private double output;
+
+    protected void setUp() throws RemoteException {
+
+	frame1 = new Frame1();
+	frame2 = new Frame2(frame1);
+	expectedFläche = 35;
+	höhe = 5;
+	breite = 7;
+	frame2.calcFläche(höhe, breite);
+
+    }
+
+    protected void tearDown() throws RemoteException {
+
+	frame1 = null;
+	frame2 = null;
+	expectedFläche = 0;
+	breite = 0;
+	höhe = 0;
+
+    }
+
+    @Test
+    public void testCalcFläche() throws RemoteException {
+	setUp();
+	actualFläche = frame2.getFläche();
+	assertEquals(expectedFläche, actualFläche, 0.0002);
+	tearDown();
+	/*
+	 * ODER: assertEquals(expectedFläche, frame2.getFläche(), 0.0002
+	 */
+    }
+
+}
